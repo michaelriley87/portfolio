435c5efac91297380e329134d1e3e42e6ceaa234
Michael Riley <michaelriley87@gmail.com>
2025-02-13 15:40:53 +1100

    small adjustments for mobile

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,8 +1,8 @@
 <template>
     <section id="contact" class="section">
         <h2 class="section-title">Contact Me</h2>
-        <p class="section-text">If you're interested in working together or have any questions, feel free to reach out.
-        </p>
+        <p class="section-text">If you're interested in working together or have any questions, feel free to reach out.</p>
+        <p class="section-text">michaelriley87@gmail.com</p>
         <a href="mailto:michaelriley87@gmail.com" class="btn">Email Me</a>
     </section>
 </template>
